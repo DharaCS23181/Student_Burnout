5d214c0a2fc73a1e204ec57cf7724b3083b25220
AnshCS23185 <anshth.cse23@sbjit.edu.in>
2026-03-24 15:27:44 +0530

    Last

--- frontend/src/Form.jsx
+++ frontend/src/Form.jsx
@@ -17,7 +17,7 @@ export default function Form() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://127.0.0.1:8000/predict", {
+    const res = await fetch("/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
